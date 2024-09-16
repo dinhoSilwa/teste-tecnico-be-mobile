d50990a1007f64291cee484c4b62a508701f1530
claudio silva <claudiosilva.one@gmail.com>
2024-09-16 13:43:56 -0300

    feat : adds querys Providers

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+const clienteQuery = new QueryClient();
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <QueryClientProvider client={clienteQuery}>
+      <App />
+    </QueryClientProvider>
+  </StrictMode>
+);
