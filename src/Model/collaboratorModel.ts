2ce61c6ebb20f5bc295f73b98cddc261de39f714
claudio silva <claudiosilva.one@gmail.com>
2024-09-16 13:48:18 -0300

    feat : adds collaborator types and models

---
+++ src/Model/collaboratorModel.ts
@@ -0,0 +1,23 @@
+import type { ItheaderItemstable } from "../Components/Form/addCollaborator";
+
+export const theaderItemstable: ItheaderItemstable[] = [
+  {
+    avatar: "Foto",
+    nome: "Nome",
+    position: "position",
+    admission: "Data de Admissão",
+    phone: "Telefone",
+    action: "Opções",
+  },
+];
+
+export const collaboratorData = [
+  {
+    avatar: "avatar",
+    name: "João",
+    position: "Dev. Frontend",
+    admission: "11/12/2023",
+    phone: "5585987596435",
+    action: "Menu",
+  },
+];
