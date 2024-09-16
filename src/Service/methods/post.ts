ea43e05b371fe3950327ffb0e070f3722f760f53
claudio silva <claudiosilva.one@gmail.com>
2024-09-16 13:46:02 -0300

    feat : post to register collaborator

---
+++ src/Service/methods/post.ts
@@ -0,0 +1,10 @@
+import { AxiosInstance } from "axios";
+import { IColalaborator } from "../../types/Employers/collabotarorType";
+
+export const addNewEmployer = async (
+  api: AxiosInstance,
+  CollaboratorData: IColalaborator
+) => {
+  const { data } = await api.post("collaborator", CollaboratorData);
+  return data;
+};
