1d64d8260ae0cdd758fd832ef9b335014a93feb4
claudio silva <claudiosilva.one@gmail.com>
2024-09-17 00:57:59 -0300

    feat : adds delete function of the personal hook

--- src/Components/Table/collaboratorTable.tsx
+++ src/Components/Table/collaboratorTable.tsx
@@ -1,8 +1,19 @@
+import { useDeleteCollaborator } from "../../Hooks/useDelete";
 import { useGetEmployerList } from "../../Hooks/useGetEmployer";
 import { theaderItemstable } from "../../Model/collaboratorModel";
 
 const CollaboratorTable = () => {
   const { data } = useGetEmployerList();
+  const { deleteMutation } = useDeleteCollaborator();
+
+  const handleDelete = (id: string, name: string) => {
+    console.log(id);
+    const confirmeDelete = confirm(`Deletar ${name} ??`);
+    if (confirmeDelete) {
+      deleteMutation.mutate(id);
+      return;
+    }
+  };
 
   return (
     <section className="deployers-table">
@@ -29,10 +40,18 @@ const CollaboratorTable = () => {
                   <img src={items.avatar} alt={items.name} />
                 </figure>
               </td>
+              <td>{items._id}</td>
               <td>{items.name}</td>
               <td>{items.position}</td>
               <td>{items.admission}</td>
               <td>{items.phone}</td>
+              <td>
+                <button
+                  onClick={() => handleDelete(items._id as string, items.name)}
+                >
+                  Delete
+                </button>
+              </td>
             </tr>
           ))}
         </tbody>
