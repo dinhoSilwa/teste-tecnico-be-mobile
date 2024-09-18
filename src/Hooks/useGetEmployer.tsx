737e64a5081e03460bf04d7c6335c164c84a17e5
claudio silva <claudiosilva.one@gmail.com>
2024-09-18 12:33:05 -0300

    refactor : modify variable names and function names

--- src/Hooks/useGetEmployer.tsx
+++ src/Hooks/useGetEmployer.tsx
@@ -1,18 +1,18 @@
 import { useHttp } from "../Service/useHttp";
 import { CollaboratorServices } from "../Service/employerServices";
 import { useQuery, type UseQueryResult } from "@tanstack/react-query";
-import type { IColalaborator } from "../types/Employers/collabotarorType";
+import type { ICollaborator } from "../types/Employers/collabotarorType";
 import type { ICollaboratorResponse } from "../Service/methods/get";
 
-const fetchAllCollaborators = async (): Promise<IColalaborator[] | any> => {
+const fetchAllCollaborators = async (): Promise<ICollaborator[] | any> => {
   const api = useHttp();
   const getList = new CollaboratorServices();
   const response: ICollaboratorResponse = await getList.AllCollaborators(api);
   return response;
 };
 
-export const useGetEmployerList = (): UseQueryResult<IColalaborator[]> => {
-  const getcollaboratorQuery = useQuery<IColalaborator[]>({
+export const useGetEmployerList = (): UseQueryResult<ICollaborator[]> => {
+  const getcollaboratorQuery = useQuery<ICollaborator[]>({
     queryFn: fetchAllCollaborators,
     queryKey: ["get-collaborator"],
   });
