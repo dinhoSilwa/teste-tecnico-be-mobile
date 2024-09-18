7978a8f35f1a9c9a247a730496dfb1c9e05e5d39
claudio silva <claudiosilva.one@gmail.com>
2024-09-18 12:38:05 -0300

    feat : (ui) adds search bar

---
+++ src/Components/SearchBar/searchBars.tsx
@@ -0,0 +1,33 @@
+import { PlusSquare, SearchIcon } from "lucide-react";
+import { isOpenFormStore } from "../../store/FormStore";
+
+export const SearchBar = () => {
+  const { isOpenForm, setIsOpenForm } = isOpenFormStore();
+
+  return (
+    <section className="h-28 flex items-center justify-between px-12 mb-8">
+      <h2 className="text-2xl font-bold text-slate-800">Colaboradores</h2>{" "}
+      <fieldset className="flex items-center">
+        <input
+          type="text"
+          id="searachCollaborators"
+          className="ring rounded-md h-8 w-96 text-xl px-2 font-bold"
+        />
+        <label
+          htmlFor="searachCollaborators"
+          className="-translate-x-8 text-blue-900"
+        >
+          <SearchIcon />
+        </label>
+
+        <button
+          type="button"
+          className="flex items-center justify-center gap-2 w-40 bg-green-600 text-white px-2 py-3 rounded-xl text-md font-bold"
+          onClick={() => setIsOpenForm(!isOpenForm)}
+        >
+          <PlusSquare /> Adicionar
+        </button>
+      </fieldset>
+    </section>
+  );
+};
