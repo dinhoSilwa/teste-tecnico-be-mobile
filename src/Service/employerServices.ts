f1f60125f5d8161ee3b67bee4df4ca17103b7911
claudio silva <claudiosilva.one@gmail.com>
2024-09-18 12:27:38 -0300

    feat : adds function that get collaborator by id

--- src/Service/employerServices.ts
+++ src/Service/employerServices.ts
@@ -1,26 +1,39 @@
 import type { AxiosInstance } from "axios";
 import { addNewEmployer } from "./methods/post";
-import type { IColalaborator } from "../types/Employers/collabotarorType";
-import { updateCollaboratorDetails } from "./methods/put";
+import type { ICollaborator } from "../types/Employers/collabotarorType";
+import { updateCollaboratorByIdDetails } from "./methods/put";
 import { deleteCollaboratorId } from "./methods/delete";
-import { getAllCollaborators, type ICollaboratorResponse } from "./methods/get";
+import {
+  collaboratorById,
+  getAllCollaborators,
+  type ICollaboratorResponse,
+} from "./methods/get";
+
+export interface collaboratorProps {
+  collaborator : ICollaborator
+}
 
 export class CollaboratorServices {
   AllCollaborators = async (
     api: AxiosInstance
   ): Promise<ICollaboratorResponse> => await getAllCollaborators(api);
 
+  getCollaboratorById = async (
+    api: AxiosInstance,
+    id: string
+  ): Promise<collaboratorProps> => await collaboratorById(api, id);
+
   createCollabotaror = async (
     api: AxiosInstance,
-    CollaboratorData: IColalaborator
+    CollaboratorData: ICollaborator
   ): Promise<void> => await addNewEmployer(api, CollaboratorData);
 
-  updateCollaborator = async (
+  updateCollaboratorById = async (
     api: AxiosInstance,
     collaboratorId: string,
-    update: IColalaborator
+    update: ICollaborator
   ): Promise<void> =>
-    await updateCollaboratorDetails(api, collaboratorId, update);
+    await updateCollaboratorByIdDetails(api, collaboratorId, update);
 
   deleteCollaborator = async (
     api: AxiosInstance,
