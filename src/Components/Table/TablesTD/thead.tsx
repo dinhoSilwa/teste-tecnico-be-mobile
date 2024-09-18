a1443a2ab40b94ae464a90ac3e89b71a0f0a6ff6
claudio silva <claudiosilva.one@gmail.com>
2024-09-18 12:36:10 -0300

    feat: (ui) Personal all and tr for collaborator table

---
+++ src/Components/Table/TablesTD/thead.tsx
@@ -0,0 +1,48 @@
+import { PenSquare, Trash2 } from "lucide-react";
+
+interface ITheadTd {
+  tdString: string;
+}
+
+interface IBodyTd {
+  tdString: string | React.ReactElement;
+}
+
+// interface TBodyTdIconProps {
+//   tdString: string | React.ReactElement;
+// }
+
+export const TheadTd = ({ tdString, ...props }: ITheadTd) => {
+  return (
+    <th className="flex-1 text-center flex justify-center" {...props}>
+      {tdString}
+    </th>
+  );
+};
+
+export const TBodyTd = ({ tdString, ...props }: IBodyTd) => {
+  return (
+    <td className="flex-1 text-center flex justify-center" {...props}>
+      {tdString}
+    </td>
+  );
+};
+
+export const TBodyTdIcon = () => {
+  return (
+    <section className="flex-1 text-center flex justify-center ">
+      <button
+        className="h-10 w-12 rounded-full flex justify-center items-center"
+        title="Editar Colaborador"
+      >
+        <PenSquare size={18} className="text-slate-600 hover:text-cyan-600 " />
+      </button>
+      <button
+        className="h-10 w-12 rounded-full flex justify-center items-center"
+        title="Deletar Colaborador"
+      >
+        <Trash2 size={18} className="text-slate-600 hover:text-red-500" />
+      </button>
+    </section>
+  );
+};
