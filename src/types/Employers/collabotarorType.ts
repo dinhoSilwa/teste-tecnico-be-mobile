ae3977374866d2905cc50a8eb2d1f7b8a9cebafe
claudio silva <claudiosilva.one@gmail.com>
2024-09-18 15:39:03 -0300

    feat : adds interface to updateprops

--- src/types/Employers/collabotarorType.ts
+++ src/types/Employers/collabotarorType.ts
@@ -1,7 +1,15 @@
+import type { AxiosInstance } from "axios";
+
 export interface ICollaborator {
   _id?: string;
   name: string;
   position: string;
   admission: string;
   phone: string;
+}
+
+export interface UpdateProps {
+  api: AxiosInstance;
+  collaboratorId: string;
+  update: ICollaborator;
 }
