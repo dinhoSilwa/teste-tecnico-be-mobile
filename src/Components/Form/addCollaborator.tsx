1b29754682f5b59c42850633bffda37521739dbe
claudio silva <claudiosilva.one@gmail.com>
2024-09-18 12:38:46 -0300

    refactor : collaborator forms

--- src/Components/Form/addCollaborator.tsx
+++ src/Components/Form/addCollaborator.tsx
@@ -1,75 +1,143 @@
+import clsx from "clsx";
 import { useFormValidation } from "../../Hooks/useformValidation";
-import { theaderItemstable } from "../../Model/collaboratorModel";
+import { positions, theaderItemstable } from "../../Model/collaboratorModel";
+import { isOpenFormStore } from "../../store/FormStore";
+import { useEffect } from "react";
+import { CollaboratorStore } from "../../store/collaboratorToForm";
 
 export interface ItheaderItemstable {
-  avatar: string;
   nome: string;
   position: string;
   admission: string;
   phone: string;
-  action: any;
 }
-export const FormEmployer = () => {
-  const { handleSubmit, watch, isError, isLoading, errors, register } =
-    useFormValidation();
+
+export const FormCollaborator = () => {
+  const { collaborator } = CollaboratorStore();
+  const { isOpenForm, setIsOpenForm } = isOpenFormStore();
+  const {
+    handleSubmit,
+    isError,
+    isLoading,
+    isSuccess,
+    errors,
+    watch,
+    reset,
+    register,
+  } = useFormValidation();
 
   const name = watch("name");
-  const avatar = watch("avatar");
   const position = watch("position");
   const phone = watch("phone");
   const admission = watch("admission");
+
+  useEffect(() => {
+    const handleCloseModal = () => {
+      if (isSuccess) {
+        setIsOpenForm(false);
+      }
+    };
+    handleCloseModal();
+  }, [isSuccess, isOpenForm]);
+
+  useEffect(() => {
+    console.log(collaborator);
+    if (collaborator) {
+      const { name, position, admission, phone } = collaborator as any;
+      if (!name || !position || !admission || !phone) {
+        console.log("Tem coisa aqui , Preparado Para Atualizar")
+        return;
+      }
+      reset({
+        name,
+        admission,
+        position,
+        phone,
+      });
+    }
+  }, [collaborator]);
+
+  const handleChoseSubmit = () => {
+    if (!collaborator) {
+      handleSubmit;
+      return;
+    }
+  };
+
   return (
-    <form onSubmit={handleSubmit}>
+    <form
+      onSubmit={handleChoseSubmit}
+      className={clsx(
+        "z-50 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-lg w-[50%] px-8 flex flex-col pt-8 h-[400px]",
+        { flex: isOpenForm === true },
+        { hidden: isOpenForm === false }
+      )}
+    >
+      <legend className="flex items-center justify-center text-cyan-900 font-bold">
+        Adicionar Novo Collaborador
+      </legend>
       {theaderItemstable.map((items, index) => (
-        <fieldset key={index}>
-          <label htmlFor={items.avatar}>{items.avatar}</label>
+        <fieldset key={index} className="flex flex-col py-2 gap-4">
           <input
             type="text"
-            id={items.avatar}
-            {...register("avatar")}
-            value={avatar || ""}
-          />
-          {errors.avatar && <span>{errors.avatar.message}</span>}
-
-          <label htmlFor={items.nome}>{items.nome}</label>
-          <input
-            type="text"
-            id={items.nome}
-            {...register("name")}
             value={name || ""}
+            placeholder={items.nome}
+            {...register("name")}
+            className={clsx("px-4 py-2 border-2 border-slate-800 rounded-md", {
+              "bg-red-100 rounded-md px-2 text-red-500": errors.name,
+            })}
           />
 
-          {errors.name && <span>{errors.name.message}</span>}
+          <datalist id="positionList" {...register("position")}>
+            {positions.map((position, index) => (
+              <option value={position} key={index} />
+            ))}
+          </datalist>
 
-          <label htmlFor={items.position}>{items.position}</label>
           <input
+            list="positionList"
             type="text"
-            id={items.position}
-            {...register("position")}
             value={position || ""}
+            placeholder={items.position}
+            {...register("position")}
+            className={clsx("px-4 py-2 border-2 border-slate-800 rounded-md", {
+              "bg-red-100 rounded-md px-2 text-red-500": errors.position,
+            })}
           />
-          {errors.position && <span>{errors.position.message}</span>}
 
-          <label htmlFor={items.admission}>{items.admission}</label>
           <input
-            type="text"
-            id={items.admission}
-            {...register("admission")}
+            type="date"
             value={admission || ""}
+            placeholder={items.admission}
+            {...register("admission")}
+            className={clsx("px-4 py-2 border-2 border-slate-800 rounded-md", {
+              "bg-red-100 rounded-md px-2 text-red-500": errors.admission,
+            })}
           />
-          {errors.admission && <span>{errors.admission.message}</span>}
 
-          <label htmlFor={items.phone}>{items.phone}</label>
           <input
             type="text"
-            id={items.phone}
-            {...register("phone")}
             value={phone || ""}
+            placeholder={items.phone}
+            {...register("phone")}
+            className={clsx("px-4 py-2 border-2 border-slate-800 rounded-md", {
+              "bg-red-100 rounded-md px-2 text-red-500": errors.phone,
+            })}
           />
-          {errors.phone && <span>{errors.phone.message}</span>}
         </fieldset>
       ))}
-      <button>{isLoading ? "Caregando..." : "Cadastrar"}</button>
+      <div className="w-full flex gap-2 justify-end my-4">
+        <button className="w-32 bg-green-600 text-white py-2 rounded-lg">
+          {isLoading ? "Caregando..." : "Cadastrar"}
+        </button>
+
+        <div
+          className="w-32 flex justify-center items-center border-2 bg-red-100 border-red-600  text-red-600 py-2 rounded-lg cursor-pointer"
+          onClick={() => setIsOpenForm(!isOpenForm)}
+        >
+          Cancelar
+        </div>
+      </div>
       {isError && <span>Falha ao Cadastrar</span>}
     </form>
   );
