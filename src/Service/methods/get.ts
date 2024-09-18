471fb1bcc5f6884a7dec61afefe48166b6fbea6b
claudio silva <claudiosilva.one@gmail.com>
2024-09-18 12:27:01 -0300

    refactor : modify get collaborator function

--- src/Service/methods/get.ts
+++ src/Service/methods/get.ts
@@ -1,13 +1,23 @@
 import type { AxiosInstance, AxiosResponse } from "axios";
-import type { IColalaborator } from "../../types/Employers/collabotarorType";
+import type { ICollaborator } from "../../types/Employers/collabotarorType";
+import type { collaboratorProps } from "../employerServices";
 
 export interface ICollaboratorResponse {
-  getAll: IColalaborator;
+  getAll: ICollaborator;
 }
 export const getAllCollaborators = async (
   api: AxiosInstance
 ): Promise<ICollaboratorResponse> => {
   const response: AxiosResponse<ICollaboratorResponse> =
     await api.get<ICollaboratorResponse>("collaborators");
+  return response.data;
+};
+
+export const collaboratorById = async (
+  api: AxiosInstance,
+  id: string
+): Promise<collaboratorProps> => {
+  const response: AxiosResponse<collaboratorProps> =
+    await api.get<collaboratorProps>(`collaborators/${id}`);
   return response.data;
 };
