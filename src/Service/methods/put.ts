15c04a83c9dd251e7da9df37fdd5513870de3379
claudio silva <claudiosilva.one@gmail.com>
2024-09-18 12:22:38 -0300

    feat : implements the function to get employee data by their id

--- src/Service/methods/put.ts
+++ src/Service/methods/put.ts
@@ -1,18 +1,18 @@
-// export const updateCollaborator = async (
+// export const updateCollaboratorById = async (
 //   api: AxiosInstance,
-//   CollaboratorData: IColalaborator
+//   CollaboratorData: ICollaborator
 // ) => {
 //   const { data } = await api.post("collaborators", CollaboratorData);
 //   return data;
 // };
 
 import type { AxiosInstance } from "axios";
-import type { IColalaborator } from "../../types/Employers/collabotarorType";
+import type { ICollaborator } from "../../types/Employers/collabotarorType";
 
-export const updateCollaboratorDetails = async (
+export const updateCollaboratorByIdDetails = async (
   api: AxiosInstance,
   collaboratorId: string,
-  update: IColalaborator
+  update: ICollaborator
 ) => {
   await api.put(`collaborators/${collaboratorId}`, update);
 };
