aa6088d682dec53cc99c22e1547a0e63746ac058
claudio silva <claudiosilva.one@gmail.com>
2024-09-18 12:38:22 -0300

    feat : ui navbar

---
+++ src/Components/Header/bar/superBar.tsx
@@ -0,0 +1,16 @@
+export const SuperBar = () => {
+  return (
+    <>
+      <nav className="w-full bg-slate-800 h-16 justify-start px-4 flex items-center">
+        <div className="flex">
+          <span className="text-sm text-white w-8 h-8 flex items-center justify-center border-2 border-cyan-800 rounded-lg">
+            Be
+          </span>
+          <span className="h-8 items-center font-semibold text-cyan-500 flex text-sm">
+            Mobile
+          </span>
+        </div>
+      </nav>
+    </>
+  );
+};
