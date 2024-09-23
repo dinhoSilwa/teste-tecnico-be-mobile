ff0c77446aaac753fe12a88962b2904cb8b92d09
claudio silva <claudiosilva.one@gmail.com>
2024-09-23 09:20:10 -0300

    feat : adds yup matches to schemas

--- src/Model/shemasYup/collaboratorSchemas.ts
+++ src/Model/shemasYup/collaboratorSchemas.ts
@@ -5,7 +5,12 @@ export const collaboratorSchema = yup.object().shape({
   //avatar: yup.string(),
   name: yup.string().required("O nome é Obrigatorio"),
   position: yup.string().required("O position é obigatório"),
-  admission: yup
-    .string().required("A data de admissão é obrigatória"),
-  phone: yup.string().required("Digite o número de Telefone"),
+  admission: yup.string().required("A data de admissão é obrigatória"),
+  phone: yup
+    .string()
+    .required("O telefone é obrigatório")
+    .matches(
+      /^\(\d{2}\) \d{1}-\d{4}-\d{4}$/,
+      "Formato de telefone inválido (XX X-XXXX-XXXX)"
+    ),
 });
