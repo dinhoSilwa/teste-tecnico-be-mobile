c27ce686af37c7d294a50b1c5c5e375e41e57eff
claudio silva <claudiosilva.one@gmail.com>
2024-09-25 14:26:47 -0300

    remove : strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
@@ -7,9 +6,7 @@ import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 const clienteQuery = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <QueryClientProvider client={clienteQuery}>
-      <App />
-    </QueryClientProvider>
-  </StrictMode>
+  <QueryClientProvider client={clienteQuery}>
+    <App />
+  </QueryClientProvider>
 );
