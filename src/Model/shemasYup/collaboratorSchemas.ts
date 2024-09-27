d45bd9f1e3b1f13dda03ed06cea0516fad37d83c
claudio silva <claudiosilva.one@gmail.com>
2024-09-27 09:07:36 -0300

    refactor : remove unused files

--- src/Model/shemasYup/collaboratorSchemas.ts
+++ src/Model/shemasYup/collaboratorSchemas.ts
@@ -2,7 +2,6 @@ import * as yup from "yup";
 
 export const collaboratorSchema = yup.object().shape({
   _id: yup.string(),
-  //avatar: yup.string(),
   name: yup.string().required("O nome é Obrigatorio"),
   position: yup.string().required("O position é obigatório"),
   admission: yup.string().required("A data de admissão é obrigatória"),
