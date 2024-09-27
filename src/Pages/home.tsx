5e522d3c2b505078155a83e58708deb8a027df60
claudio silva <claudiosilva.one@gmail.com>
2024-09-27 12:16:36 -0300

    refactor : [ui] remove background modal

--- src/Pages/home.tsx
+++ src/Pages/home.tsx
@@ -1,8 +1,8 @@
-import clsx from "clsx";
 import { FormCollaborator } from "../Components/Form/addCollaborator";
 import { SuperBar } from "../Components/Header/bar/superBar";
 import { SearchBar } from "../Components/SearchBar/searchBars";
 import CollaboratorTable from "../Components/Table/collaboratorTable";
+import { ModalForm } from "../Components/Modal/modal";
 import { isOpenFormStore } from "../store/FormStore";
 
 export const Home = () => {
@@ -11,15 +11,9 @@ export const Home = () => {
   return (
     <>
       <section>
+        {isOpenForm && <ModalForm children={<FormCollaborator />} />}
         <SuperBar />
         <SearchBar />
-        <section
-          className={clsx(
-            "z-40 background h-screen w-screen  inset-0 bg-slate-800/50 backdrop-blur-sm fixed top-0 left-0",
-            { hidden: isOpenForm === false }
-          )}
-        ></section>
-        <FormCollaborator />
         <CollaboratorTable />
       </section>
     </>
