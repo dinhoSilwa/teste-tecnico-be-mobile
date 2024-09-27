34e8ec369f0e4a339b72c07cb626cd85f68df665
claudio silva <claudiosilva.one@gmail.com>
2024-09-27 12:17:47 -0300

    refactor : change toggle form template type

--- src/Components/Form/addCollaborator.tsx
+++ src/Components/Form/addCollaborator.tsx
@@ -1,59 +1,53 @@
 import clsx from "clsx";
 import { positions, theaderItemstable } from "../../Model/collaboratorModel";
-import { isOpenFormStore } from "../../store/FormStore";
-import { useEffect, useState } from "react";
 import { useFormMutation } from "../../Hooks/useformValidation";
 import { CollaboratorStore } from "../../store/collaboratorToForm";
 import InputMask from "react-input-mask";
+import { isOpenFormStore } from "../../store/FormStore";
+import { useEffect } from "react";
 
 export const FormCollaborator = () => {
-  const { isOpenForm, setIsOpenForm } = isOpenFormStore();
   const { collaborator, clear } = CollaboratorStore();
-  const [formMode, setFormMode] = useState<"add" | "edit">("add");
-
-  console.log(formMode);
-  const { handleSubmit, isError, isLoading, errors, watch, register, reset } =
-    useFormMutation(collaborator._id ? "edit" : "add", collaborator?._id);
-
-  useEffect(() => {
-    if (collaborator._id) {
-      setFormMode("edit");
-      reset({
-        name: collaborator.name,
-        admission: collaborator.admission,
-        position: collaborator.position,
-        phone: collaborator.phone,
-      });
-    } else {
-      setFormMode("add");
-      reset();
-      clear();
-    }
-  }, [collaborator._id, reset]);
-
-  // useEffect(() => {
-  //   if (isSuccess) {
-  //     setIsOpenForm(false);
-  //   }
-  //   if(formMode === "add"){
-  //     reset()
-  //     clear()
-  //   }
-
-  // }, [isSuccess, reset, clear, setIsOpenForm, isOpenForm]);
+  const { isOpenForm, setIsOpenForm } = isOpenFormStore();
+  const {
+    handleSubmit,
+    isError,
+    isLoading,
+    errors,
+    watch,
+    register,
+    reset,
+    isSuccess,
+  } = useFormMutation(collaborator._id ? "edit" : "add", collaborator?._id);
 
   const name = watch("name");
   const position = watch("position");
   const phone = watch("phone");
   const admission = watch("admission");
 
+  useEffect(() => {
+    if (!collaborator._id) reset();
+    else if (collaborator._id) {
+      reset({
+        name: collaborator.name,
+        admission: collaborator.admission,
+        position: collaborator.position,
+        phone: collaborator.phone,
+      });
+    }
+
+    return () => {
+      reset();
+      clear();
+    };
+  }, [reset]);
+  if (isSuccess) setIsOpenForm(!isOpenForm);
+
   return (
     <form
       onSubmit={handleSubmit}
       className={clsx(
-        "z-50 fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-lg w-[50%] px-8 flex flex-col pt-8 h-[400px]",
-        { flex: isOpenForm },
-        { hidden: !isOpenForm }
+        "fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-lg w-[50%] px-8 flex flex-col pt-8 h-[400px]"
       )}
     >
       <legend className="flex items-center justify-center text-cyan-900 font-bold">
@@ -119,18 +113,15 @@ export const FormCollaborator = () => {
         >
           {isLoading
             ? "Carregando..."
-            : formMode === "edit"
-            ? "Atualizar"
+            : collaborator._id
+            ? "Editar"
             : "Cadastrar"}
         </button>
-        {formMode}
 
         <button
           type="button"
           className="w-32 flex justify-center items-center border-2 bg-red-100 border-red-600 text-red-600 py-2 rounded-lg cursor-pointer"
-          onClick={() => {
-            setIsOpenForm(false);
-          }}
+          onClick={() => setIsOpenForm(!isOpenForm)}
         >
           Cancelar
         </button>
